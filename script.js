6dbf3d4534a8adf4094c5e090f07effb238ebcf1
JaypeeLan <jplaniran01@gmail.com>
2023-02-20 14:07:04 +0100

    file setup

---
+++ script.js
@@ -0,0 +1,22 @@
+const menuBars = document.getElementById("menu-bars");
+const overlay = document.getElementById("overlay");
+const nav1 = document.getElementById("nav-1");
+const nav2 = document.getElementById("nav-2");
+const nav3 = document.getElementById("nav-3");
+const nav4 = document.getElementById("nav-4");
+const nav5 = document.getElementById("nav-5");
+
+function toggleNav() {
+  // Toggle nav bars
+  menuBars.classList.toggle("change");
+  //  toggle the navigstion slide in and out
+  overlay.classList.toggle("overlay-slide-in");
+}
+
+// Event listners
+menuBars.addEventListener("click", toggleNav);
+nav1.addEventListener("click", toggleNav);
+nav2.addEventListener("click", toggleNav);
+nav3.addEventListener("click", toggleNav);
+nav4.addEventListener("click", toggleNav);
+nav5.addEventListener("click", toggleNav);
